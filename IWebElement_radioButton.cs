481b254ca4fc4624cfff718bb8b36b8b076aa745
Henrique Martins <henrique.m_s@hotmail.com>
2017-05-17 11:22:43 -0300

    Create IWebElement_radioButton.cs

---
+++ IWebElement_radioButton.cs
@@ -0,0 +1,44 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement radioButton;
+
+    static void Main()
+    {
+
+        ChromeOptions options = new ChromeOptions();
+        IWebDriver driver = new ChromeDriver();
+
+
+        string url1 = "http://testing.todvachev.com/special-elements/radio-button-test/";
+
+        string[] option = { "1", "3", "5" };
+
+        driver.Navigate().GoToUrl(url1);
+
+        for (int i = 0; i < option.Length; i++)
+        {
+            radioButton = driver.FindElement(By.CssSelector("#post-10 > div > form > p:nth-child(6) > input[type=\"radio\"]:nth-child(" + option[i] + ")"));
+            if (radioButton.GetAttribute("checked") == "true")
+            { Console.WriteLine("The " + (i + 1) + " radio button is checked!"); }
+            else
+            { Console.WriteLine("This is one of the unchecked radio buttons!"); }
+        }
+
+        Thread.Sleep(9000);
+
+        driver.Quit();
+
+
+    }
+   
+   
+
+}
+
