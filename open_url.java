5bfce135389310ddf573bdc711fe9bd2c87b056a
Henrique Martins <henrique.m_s@hotmail.com>
2017-05-29 17:03:09 -0300

    Create open_url.java

---
+++ open_url.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyClass
+{
+public static void main(String[] args)
+{
+WebDriver driver = new FirefoxDriver();
+driver.get("http://google.com");
+System.out.println(driver.getTitle());
+
+}
+}
